320ac155143a83735e82f83dffb378e64b5fdf5f
Dnyaneshwar Dhatrak <dhatrakdnyaneshwar52@gmail.com>
2023-04-27 11:36:09 +0530

    Add project files.

---
+++ Controllers/DadsJokeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DadsJokeController : ControllerBase
+    {
+        private readonly IDadsJokeInterface dadsJokeInterface;
+        public DadsJokeController(IDadsJokeInterface _dadsJokeInterface)
+        {
+            dadsJokeInterface = _dadsJokeInterface;
+        }
+
+        // GET: api/<DadsJokeController>
+        [HttpGet]
+        public async Task<string> GetDadsJoke()
+        {
+            var data = await dadsJokeInterface.GetDadsJoke();
+            return data;
+        }
+    }
+}
---
+++ Services/DadsJokeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public class DadsJokeService : IDadsJokeInterface
+    {
+        public DadsJokeService()
+        {
+
+        }
+
+        public async Task<string> GetDadsJoke()
+        {
+            string result = "";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://dad-jokes.p.rapidapi.com/random/joke"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "050010e3e5msha13df771d68d33dp16075ajsn666c38a9761a" },
+                    { "X-RapidAPI-Host", "dad-jokes.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                result = body.ToString();
+            }
+
+            return result;
+        }
+    }
+}
